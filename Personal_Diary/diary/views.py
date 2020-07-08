873cb370fce3a1fc94850e77c92efc203e4a5cec
Aman Kumar Singh <amankumar09jul@gmail.com>
2020-07-08 21:01:52 +0530

    added registration page

--- Personal_Diary/diary/views.py
+++ Personal_Diary/diary/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from .models import Diary
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,auth 
 from django.contrib.auth import logout,authenticate,login
 
 # Create your views here.
@@ -29,13 +29,27 @@ def login(request):
 		user= authenticate(username=username, password= password)
 		if user is not None:
 			login(user,request)
-			return render("/")
+			return redirect("/")
 		else:
-			return render('login.html')
-
-
+			return render(request,'/login')
 
 	return render(request,'credentials.html')
 
 # def logout(request):
 # 	return render()
+def registration(request):
+	if request.method == 'POST':
+		first_name=request.POST['first_name']
+		last_name=request.POST['last_name']
+		username=request.POST['username']
+		email=request.POST['email']
+		password1=request.POST['password1']
+		password2=request.POST['password2']
+
+		user= User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
+		user.save();
+		print("User created !")
+		return redirect('/')
+		
+	else:
+		return render(request,'registration.html')
